f125aa394743fbf043ca77f77ca3828fee8dbfc7
Nickolas Ribeiro <nickolasleosantos@hotmail.com>
2024-08-10 01:52:30 -0300

    fix(db): implements uuid

--- database/migrations/2024_08_10_045125_create_categories_table.php
+++ database/migrations/2024_08_10_045125_create_categories_table.php
@@ -11,13 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->uuid()->primary();
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
             $table->timestamps();
         });
     }
@@ -27,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('categories');
     }
 };
