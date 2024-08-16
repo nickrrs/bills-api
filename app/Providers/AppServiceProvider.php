959b3ba16da3e3774df9b09a6750da24435bfd45
Nickolas Ribeiro <nickolasleosantos@hotmail.com>
2024-08-16 16:31:33 -0300

    feature: implements an observer to always create a default account for a new user and uui personal tokens

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,13 @@
 
 namespace App\Providers;
 
+use App\Models\{PersonalAccessToken, User};
+use App\Observers\{UserObserver};
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Validation\Rules\Password;
+use Laravel\Sanctum\Sanctum;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +25,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Sanctum::usePersonalAccessTokenModel(PersonalAccessToken::class);
+
+        User::observe(UserObserver::class);
+
+        Password::defaults(function () {
+            $rule = Password::min(8);
+
+            return $this->app->environment(['production'])
+                ? $rule->mixedCase()->numbers()->symbols()->uncompromised()
+                : $rule;
+        });
+
+        Model::preventLazyLoading();
+
+        JsonResource::withoutWrapping();
     }
 }
