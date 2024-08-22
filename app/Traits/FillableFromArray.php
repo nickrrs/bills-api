39efebefe440cd6d599cee2ceb15663b5d8f2b99
Nickolas Ribeiro <nickolasleosantos@hotmail.com>
2024-08-22 12:54:16 -0300

    feature(trait): fillable from array trait
    This new trait allows updates all resources by retrieving the property names from the previous object class and handle all data manipulation.

---
+++ app/Traits/FillableFromArray.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Traits;
+
+trait FillableFromArray {
+    public function fillFromArray(array $data, array $existingData = []): void
+    {
+        foreach (get_object_vars($this) as $propertyName => $value) {
+            $this->$propertyName = $data[$propertyName] ?? $existingData[$propertyName] ?? $value;
+        }
+    }
+}
